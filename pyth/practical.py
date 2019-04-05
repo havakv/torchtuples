f247046bda5261901865156ff464682bb74f7adb
havakv <haavard.kvamme@gmail..com>
2019-04-05 12:55:09 +0200

    Bug, forgot activation

--- pyth/practical.py
+++ pyth/practical.py
@@ -15,17 +15,17 @@ def init_embedding(emb):
 
 class LinearVanillaBlock(nn.Module):
     def __init__(self, in_features, out_features, bias=True, batch_norm=True, dropout=0., activation=nn.ReLU,
-                 w_init=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
+                 w_init_=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
         super().__init__()
         self.linear = nn.Linear(in_features, out_features, bias)
-        if w_init:
-            w_init(self.linear.weight.data)
+        if w_init_:
+            w_init_(self.linear.weight.data)
         self.activation = activation()
         self.batch_norm = nn.BatchNorm1d(out_features) if batch_norm else None
         self.dropout = nn.Dropout(dropout) if dropout else None
 
     def forward(self, input):
-        input = self.linear(input)
+        input = self.activation(self.linear(input))
         if self.batch_norm:
             input = self.batch_norm(input)
         if self.dropout:
@@ -36,14 +36,14 @@ class LinearVanillaBlock(nn.Module):
 class MLPVanilla(nn.Module):
     def __init__(self, in_features, num_nodes, out_features, batch_norm=True, dropout=None, activation=nn.ReLU,
                  output_activation=None, output_bias=True,
-                 w_init=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
+                 w_init_=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
         super().__init__()
         num_nodes = tuplefy(in_features, num_nodes).flatten()
         if not hasattr(dropout, '__iter__'):
             dropout = [dropout for _ in range(len(num_nodes)-1)]
         net = []
         for n_in, n_out, p in zip(num_nodes[:-1], num_nodes[1:], dropout):
-            net.append(LinearVanillaBlock(n_in, n_out, True, batch_norm, p, activation, w_init))
+            net.append(LinearVanillaBlock(n_in, n_out, True, batch_norm, p, activation, w_init_))
         net.append(nn.Linear(num_nodes[-1], out_features, output_bias))
         if output_activation:
             net.append(output_activation)
@@ -83,12 +83,12 @@ class MixedInputMLP(nn.Module):
     def __init__(self, in_features, num_embeddings, embedding_dims, num_nodes, out_features,
                  batch_norm=True, dropout=None, activation=nn.ReLU, dropout_embedding=0.,
                  output_activation=None, output_bias=True,
-                 w_init=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
+                 w_init_=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
         super().__init__()
         self.embeddings = EntityEmbeddings(num_embeddings, embedding_dims, dropout_embedding)
         input_mlp = in_features + sum(embedding_dims)
         self.mlp = MLPVanilla(input_mlp, num_nodes, out_features, batch_norm, dropout, activation,
-                              output_activation, output_bias, w_init)
+                              output_activation, output_bias, w_init_)
 
     def forward(self, input_numeric, input_categoric):
         input = torch.cat([input_numeric, self.embeddings(input_categoric)], 1)
