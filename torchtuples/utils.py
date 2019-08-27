a81e95b3e334a1f69af053384a1288e4fb2f4268
Haavard Kvamme <haavard.kvamme@gmail.com>
2019-08-27 17:51:50 +0200

    Bug windows path (#4)
    * Changed name hash to not include colon (to work on windows)
    
    * Fixed windows path problems

--- torchtuples/utils.py
+++ torchtuples/utils.py
@@ -2,13 +2,11 @@ import time
 import random
 
 def make_name_hash(name='', file_ending='.pt'):
-    timestamp = time.ctime().replace(' ', '_')
-    nanoseconds = str(time.time_ns())[-9:]
+    year, month, day, hour, minute, second = time.localtime()[:6]
     ascii_letters_digits = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    random_hash = ''.join(random.choices(ascii_letters_digits, k=10))
-    path = f"{name}_{timestamp}_{nanoseconds}_{random_hash}{file_ending}"
+    random_hash = ''.join(random.choices(ascii_letters_digits, k=20))
+    path = f"{name}_{year}-{month}-{day}_{hour}-{minute}-{second}_{random_hash}{file_ending}"
     return path
-
 
 class TimeLogger:
     def __init__(self, start=None):
