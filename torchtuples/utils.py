6fb054b67d66248c59df23de66422f6d6cbececc
havakv <haavard.kvamme@gmail..com>
2019-12-03 15:24:51 +0100

    Added array_or_tensor for function outputs

--- torchtuples/utils.py
+++ torchtuples/utils.py
@@ -1,5 +1,8 @@
 import time
 import random
+import numpy as np
+import torch
+from torchtuples import tuplefy, TupleTree
 
 def make_name_hash(name='', file_ending='.pt'):
     year, month, day, hour, minute, second = time.localtime()[:6]
@@ -47,3 +50,37 @@ class TimeLogger:
         hms_start = self._hms_from_sec(diff_start)
         hms_prev = self._hms_from_sec(diff_prev)
         return self._hms_str(*hms_start, shorten), self._hms_str(*hms_prev, shorten)
+
+
+def array_or_tensor(tensor, numpy, input):
+    """Returs a tensor if numpy is False or input is tensor.
+    Else it returns numpy array, even if input is a DataLoader.
+    """
+    is_tensor = None
+    if numpy is False:
+        is_tensor = True
+    elif (numpy is True) or is_dl(input):
+        is_tensor = False
+    elif not (is_data(input) or is_dl(input)):
+        raise ValueError(f"Do not understand type of `input`: {type(input)}")
+    elif tuplefy(input).type() is torch.Tensor:
+        is_tensor = True
+    elif tuplefy(input).type() is np.ndarray:
+        is_tensor = False
+    else:
+        raise ValueError("Something wrong")
+    
+    if is_tensor:
+        tensor = tuplefy(tensor).to_tensor().val_if_single()
+    else:
+        tensor = tuplefy(tensor).to_numpy().val_if_single()
+    return tensor
+
+def is_data(input):
+    """Returns True if `input` is data of type tuple, list, TupleTree, np.array, torch.Tensor."""
+    datatypes = [np.ndarray, torch.Tensor, tuple, list, TupleTree]
+    return any([isinstance(input, ct) for ct in datatypes])
+
+def is_dl(input):
+    """Returns True if `input` is a DataLoader (inherit from DataLoader)."""
+    return isinstance(input, torch.utils.data.DataLoader)
