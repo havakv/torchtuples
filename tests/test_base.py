6796075c3f29dab69dcd0a5daf83017efaf8fdf8
havakv <haavard.kvamme@gmail..com>
2019-12-03 19:38:28 +0100

    Added tests and docs for prediction

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
-from torchtuples import optim, Model, TupleTree
+from torch import nn
+from torchtuples import optim, Model, TupleTree, tuplefy
 from torchtuples.testing import assert_tupletree_equal
 
 
@@ -8,6 +9,9 @@ class TestModel:
     def setup(self):
         torch.manual_seed(1234)
         self.net = torch.nn.Linear(10, 3)
+        x = torch.randn(4, 10)
+        y = torch.randn(4, 3)
+        self.data = tuplefy(x, y)
 
     @pytest.mark.parametrize('optim_class', [
         optim.SGD,
@@ -37,3 +41,72 @@ class TestModel:
         assert type(model.optimizer) is optim.OptimWrap
         assert type(model.optimizer.optimizer) is optimizer_class
         assert model.optimizer.param_groups[0]['lr'] == lr
+
+    def test_score_in_batches(self):
+        model = Model(self.net, torch.nn.MSELoss())
+        a = model.score_in_batches(*self.data)
+        b = model.score_in_batches(self.data)
+        assert a == b
+
+
+class _PredSigmoidNet(nn.Module):
+    def __init__(self, net):
+        super().__init__()
+        self.net = net
+
+    def forward(self, input):
+        return self.net(input)
+
+    def predict(self, input):
+        return torch.sigmoid(self.net(input))
+
+class TestPrediction:
+    def setup(self):
+        torch.manual_seed(1234)
+        self.net = torch.nn.Linear(5, 3)
+        self.prednet = _PredSigmoidNet(self.net)
+        x = torch.randn(4, 5)
+        self.data = x
+        self.dataloader = tuplefy(x).make_dataloader(5, False)
+        self.net_dropout = nn.Sequential(self.net, nn.Dropout(0.5))
+
+    @pytest.mark.parametrize('dl', (True, False))
+    @pytest.mark.parametrize('numpy', (True, False, None))
+    def test_predict_with_fun(self, dl, numpy):
+        data = self.dataloader if dl else self.data
+        model = Model(self.net, nn.MSELoss())
+        model_pred = Model(self.prednet, nn.MSELoss())
+        a = model.predict(data, func=torch.sigmoid, numpy=numpy)
+        b = model_pred.predict(data, numpy=numpy)
+        a_net = model.predict_net(data, func=torch.sigmoid, numpy=numpy)
+        b_net = model_pred.predict_net(data, func=torch.sigmoid, numpy=numpy)
+        assert (a == b).all()
+        assert (a_net == b_net).all()
+        assert (a == a_net).all()
+
+    @pytest.mark.parametrize('dl', (True, False))
+    @pytest.mark.parametrize('numpy', (True, False, None))
+    def test_predict_no_fun(self, dl, numpy):
+        data = self.dataloader if dl else self.data
+        model = Model(self.net, nn.MSELoss())
+        model_pred = Model(self.prednet, nn.MSELoss())
+        a = model.predict(data, numpy=numpy)
+        b = model_pred.predict(data, numpy=numpy)
+        a_net = model.predict_net(data, numpy=numpy)
+        b_net = model_pred.predict_net(data, numpy=numpy)
+        assert not (a == b).all()
+        assert (a_net == b_net).all()
+        assert (a == a_net).all()
+
+    @pytest.mark.parametrize('eval_', (True, False))
+    def test_predict_net_eval_(self, eval_):
+        torch.manual_seed(0)
+        model = Model(self.net_dropout)
+        assert model.net.training is True
+        pred = model.predict(self.data, eval_=eval_)
+        assert model.net.training is True
+        pred2 = model.predict(self.data, eval_=eval_)
+        assert model.net.training is True
+        assert (pred == pred2).all().item() is eval_
+
+
